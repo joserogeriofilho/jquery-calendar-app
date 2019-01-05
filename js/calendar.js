315b7c7d6a92a2443358924f43259c82d2792346
Roger <roger.sama@gmail.com>
2019-01-05 03:51:07 -0300

    Created functions for filling the calendar month and days. Also, it creates a mark on the current day

--- js/calendar.js
+++ js/calendar.js
@@ -1,17 +1,52 @@
-function appendDays(){
-    let i;
+var currentDate = new Date();
+
+var currentYear = currentDate.getFullYear();
+var currentMonth = currentDate.getMonth();
+var currentDay = currentDate.getDate();
+
+var firstDay = new Date(currentYear, currentMonth, 1);
+var lastDay = new Date(currentYear, currentMonth+1, 0);
+var startingWeekDay = firstDay.getDay();
+
+var days = [];
+
+
+function fillMonth(){
+    let months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
+
+    $(".calendar-title").append("<h6>"+months[currentMonth]+"</h6>");
+}
+
+function fillDays(){
+    let i,j;
 
     for(i = 0; i < 35; i++){
-        $(".calendar-row-" + (i%7 + 1)).append("<div class='calendar-day'></div>");
+        let d = document.createElement("div");
+        d.className = "calendar-day";
+        days.push(d);
     }
+
+    for(i=1, j = startingWeekDay; j < lastDay.getDate()+startingWeekDay; i++, j++){
+        days[j].innerHTML = "<h6>"+i+"</h6>";
+    }
+
+    let currentDayMark = document.createElement("div");
+    currentDayMark.className = "current-day-mark";
+    
+    days[startingWeekDay+currentDay-1].appendChild(currentDayMark);
+    days[startingWeekDay+currentDay-1].firstChild.className ="current-day";
+
+    $(".calendar-body").append(days);
 }
 
 $(function(){
     
-    appendDays();
+    fillMonth();
 
-    $(".test").hover(function(){
+    fillDays();
+
+    /*$(".test").hover(function(){
         $(".test").hide();
-    });
+    });*/
   
   });
