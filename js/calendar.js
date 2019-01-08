aff3457d33cc675096eba0967d37a3ead51475c9
Roger <roger.sama@gmail.com>
2019-01-07 23:05:04 -0300

    Attempt to implement all the months of the year

--- js/calendar.js
+++ js/calendar.js
@@ -6,9 +6,9 @@ let currentYear = currentDate.getFullYear();
 let currentMonth = currentDate.getMonth();
 let currentDay = currentDate.getDate();
 
-let startingWeekDay = new Date(currentYear, currentMonth, 1).getDay();
-
 let selectedIndex;
+let selectedMonth = currentMonth;
+let startingWeekDay = new Date( currentYear, selectedMonth, 1 ).getDay();
 
 let days = [];
 let appointments = [];
@@ -21,103 +21,127 @@ class Appointment {
 }
 
 function fillMonth() {
-    let calendarHeader = $(".calendar-header");
-    calendarHeader.empty();
-    calendarHeader.append("<h6>"+months[currentMonth]+"</h6>");
+    $( "#month-title" ).text(months[currentMonth]);
 }
 
 function fillDays() {
     let i,j=1, firstDayofMonth, calendarBody;
 
-    firstDayofMonth = new Date(currentYear, currentMonth+1, 0).getDate();
+    firstDayofMonth = new Date( currentYear, selectedMonth+1, 0 ).getDate();
 
-    calendarBody = $(".calendar-body");
+    calendarBody = $( ".calendar-body" );
     calendarBody.empty();
 
-    for (i = 0; i < 35; i++) {
-        let d = document.createElement("div");
+    for ( i = 0; i < 35; i++ ) {
+        let d = document.createElement( "div" );
         d.className = "calendar-day";
 
-        if(i >= startingWeekDay && i < firstDayofMonth+startingWeekDay){
-            j >= currentDay && d.classList.add("available-day");
-            d.innerHTML = "<h6>"+j+"</h6><i id='day-icon' class='material-icons hover-icon'>add_circle</i>";
+        if( i >= startingWeekDay && i < firstDayofMonth+startingWeekDay ) {
+            j >= currentDay && d.classList.add( "available-day" );
+
+            d.innerHTML = "<h6>" + j + "</h6>"
+                + "<i id='day-icon' class='material-icons hover-icon'>add_circle</i>";
+
             j++;
         }
 
         days.push(d);
     }
 
-    let currentDayMark = document.createElement("div");
+    let currentDayMark = document.createElement( "div" );
     currentDayMark.className = "current-day-mark";
 
-    days[startingWeekDay+currentDay-1].appendChild(currentDayMark);
-    days[startingWeekDay+currentDay-1].firstChild.className ="current-day";
+    days[startingWeekDay+currentDay-1].appendChild( currentDayMark );
+    days[startingWeekDay+currentDay-1].firstChild.className = "current-day";
 
-    calendarBody.append(days);
+    calendarBody.append( days );
+}
+
+function getDayIcon(){
+    return $( ".calendar-day:nth-child(" + ( selectedIndex + 1 ) + ") #day-icon" );
 }
 
 function closeDialog() {
-    $(".scrim").removeClass("visible");
+    $( ".scrim" ).removeClass( "visible" );
 }
 
 $( function() {    
     fillMonth();
-
     fillDays();
 
     // Events
-    $(".form-group").on("focusin", function() {
-        $(this).addClass("has-focus");
+
+    $( "#previous-month" ).click( function() {
+        selectedMonth--;
+        startingWeekDay = new Date( currentYear, selectedMonth, 1 ).getDay();
+
+        fillMonth();
+        fillDays();
     });
 
-    $(".form-group").on("focusout", function() {
-        $(this).removeClass("has-focus");
+    $( "#next-month" ).click( function() {
+        selectedMonth++;
+        startingWeekDay = new Date( currentYear, selectedMonth, 1 ).getDay();
+
+        fillMonth();
+        fillDays();
     });
 
-    $(".available-day").click(function() {
-        selectedIndex = $(this).index();
+
+    $( "body" ).on( "click", ".available-day", function() {
+        selectedIndex = $( this ).index();
+
         let selectedDay = selectedIndex-startingWeekDay+1;
+        let calendaTitle = selectedDay + " of " + months[currentMonth] + "</h6>";
 
-        $(".dialog-appointment-header").html("<h6>"+selectedDay+" of "+months[currentMonth]+"</h6>");
-        $(".scrim").addClass("visible");
+        $( ".dialog-appointment-header" ).html( calendaTitle );
+        $( ".scrim" ).addClass( "visible" );
 
         let a = appointments[selectedIndex];
 
-        if(a != null){
-            $("#title").val(a.title);
-            $("#description").val(a.description);
+        if( a != null ){
+            $( "#title" ).val( a.title );
+            $( "#description" ).val( a.description );
         }else{
-            $("#title").val("");
-            $("#description").val("");
+            $( "#title" ).val( "" );
+            $( "#description" ).val( "" );
         }
     });
 
-    $("#btn-delete").click(function() {
+    $( "#btn-cancel" ).click(function() {
+        closeDialog();
+    });
+
+    $( "#btn-delete" ).click(function() {
         appointments[selectedIndex] = null;
 
-        let eventiIcon = $(".calendar-day:nth-child("+(selectedIndex+1)+") #day-icon");
+        let dayIcon = getDayIcon();
 
-        eventiIcon.html("add_circle");
-        eventiIcon.removeClass("fixed-icon");
-        eventiIcon.addClass("hover-icon");
+        dayIcon.html( "add_circle" );
+        dayIcon.removeClass( "fixed-icon" );
+        dayIcon.addClass( "hover-icon" );
 
         closeDialog();
     });
 
-    $("#btn-cancel").click(function() {
+    $( "#btn-save" ).click(function() {
+        appointments[selectedIndex] = new Appointment( $( "#title" ).val(), $( "#description" ).val() );
+
+        let dayIcon = getDayIcon();
+
+        dayIcon.html("event");
+        dayIcon.removeClass("hover-icon");
+        dayIcon.addClass("fixed-icon");
+
         closeDialog();
     });
 
-    $("#btn-save").click(function() {
-        appointments[selectedIndex] = new Appointment($("#title").val(), $("#description").val());
+    $( ".form-group" ).on( "focusin" , function() {
+        $( this ).addClass( "has-focus" );
+    });
 
-        let eventiIcon = $(".calendar-day:nth-child("+(selectedIndex+1)+") #day-icon");
-
-        eventiIcon.html("event");
-        eventiIcon.removeClass("hover-icon");
-        eventiIcon.addClass("fixed-icon");
-
-        closeDialog();
+    $( ".form-group" ).on( "focusout", function() {
+        $( this ).removeClass( "has-focus" );
     });
   
   });
